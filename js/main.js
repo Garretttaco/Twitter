3c65f3b6f45cee5522f582a9d7f16027a95d9362
Garrett Tacoronte <garretttacoronte@gmail.com>
2015-03-12 11:39:43 -0700

    Twitter

--- js/main.js
+++ js/main.js
@@ -1,77 +1,85 @@
-//My user class that has my username and picture
-var User = {
-	username: '@Garretttaco',
-	profile_pic: 'images/g.JPG'
-}
+$(document).ready(function() {
 
-var renderThread = function(message) {
-	var source = $('#thread-template').html();
-	var template = Handlebars.compile(source);
-	return template({
-	tweet: renderTweet(message)
+	var User = {
+		'username'	: '@Garretttaco',
+		'profile_pic': 'images/g.JPG'
+	}
+
+	//STATE MANAGEMENT
+	$(document).on('click', '.tweet', function(){
+		$(this).parents('.thread').toggleClass("expand");
 	});
-}
-
-var renderTweet = function(message, username, profile_pic) {
-	var source = $('#tweet-template').html();
-	var template = Handlebars.compile(source);
-	return template({
-	message: message,
-	username: User.username,
-	profile_pic: User.profile_pic
-	});
-}
-
-var renderCompose = function() {
-	var source = $('#compose-template').html();
-	var template = Handlebars.compile(source);
-}
-
-
-
-$(function(){
-
-	//State Management for all Compose textareas
-	$(document).on('click', '.compose', function(){
-		$(this).toggleClass('expand');
+	$(document).on('click', '.compose textarea', function(){
+		$(this).parents('.compose').toggleClass("expand");
 	});
 
-	//State Management of all replies
-	$(document).on('click','.tweet', function(){
-		$(this).parents('.thread').toggleClass('expand');
-	});
+	//FUNCTIONS
+	var makeThread = function(message, User) {
+		var source = $('#template-thread').html();
+		var template = Handlebars.compile(source);
+		return template({
+			'tweet': makeTweet(message, User),
+			'compose': makeCompose()
+		});
+	}
+ 	var makeCompose = function() {
+		var source = $('#template-compose').html();
+		var template = Handlebars.compile(source);
+		return template();
+	}
+	var makeTweet = function(message, User) {
+		var source = $('#template-tweet').html();
+		var template = Handlebars.compile(source);
+		return template({
+			'username': User.username,
+			'profile_pic': User.profile_pic,
+			'message': message
+		});
+	}
 
 
-	// Counting how many characters left in textarea starting at 140
-	$('textarea').keyup(function () {
-    var left = 140 - $(this).val().length;
-    if (left < 0) {
-        left = 0;
-    }
-    //displaying how many characters are left
-    $('.count').text(left);
-	});
+	//COMPOSE NEW TWEET
 
-	// Calling to function renderThread which creates a new thread when clicking on the submit button
-	$('header form').on('submit', function(event){
-		//deactivate the button from submitting
+	$('header').on('submit', 'form', function(event){
 		event.preventDefault();
-		//Call the function renderThread and pass the value that was input in textarea
-		var threadHTML = renderThread($('textarea').val());
-		//assigning the textarea to a variable so that it is easier for JQuery to identify
-		var tweet_text = $('textarea');
-		//if the length is greater than 0, then prepend to .tweets
-		if(tweet_text.val().length) {
+			
+		var message = $('form textarea').val();
+		if(message.length > 0){
+			var threadHTML = makeThread(message, User);
 			$('.tweets').prepend(threadHTML);
+			$('form textarea').val('');
+			$(this).parents('.compose').toggleClass("expand");
 		}
-		//Setting the textarea to an empty string after it is submitted 
-		tweet_text.val('');
 
 	});
 
+	//COMPOSE REPLY
+	$('.tweets').on('click', 'button', function(event){
+		event.preventDefault();
+		var message = $(this).parents('.compose').find('textarea').val();
+		if(message.length > 0){
+			var tweetHTML = makeTweet(message, User);
+			$(this).parents('.replies').append(tweetHTML);
+			$('form textarea').val('');
+			$(this).parents('.compose').toggleClass("expand");
+		}
+	});
 
-	//This is where i am selecting the 
+$('textarea').on('keydown', function(event){
+
+		// Get ength of message
+		var msg_lnth = $(this).val().length;
 
 
+		// Find Parent compose
+		var p = $(this).parents('.compose');
 
+
+		// Find child in compose called count
+		var c = $('.count', p);
+		console.log(c.text()- msg_lnth);
+		//update text in count to be 140 - length of message
+		c.text(140 - msg_lnth);
+
+	});	
 });
